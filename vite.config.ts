7015d934a663ab8c80c60bbb7d1b266a1be3e291
Pradeep Kumar <105277044+pradeepatwork@users.noreply.github.com>
2026-01-12 00:25:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   base: './', // Ensures assets load correctly on GitHub Pages sub-paths
   build: {
     outDir: 'dist',
